f9bcfb6e3ba6103c7a5697e2cc15b986ec192166
Martyna Gil <martynaa.gil@gmail.com>
2020-12-25 18:30:12 +0100

    initialized project

---
+++ src/main/java/com/github/martynagil/drugstoremanagement/DrugstoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.github.martynagil.drugstoremanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrugstoreManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrugstoreManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/martynagil/drugstoremanagement/DrugstoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.martynagil.drugstoremanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
