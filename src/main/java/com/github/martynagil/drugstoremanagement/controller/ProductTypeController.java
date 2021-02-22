b7e64a37e865bf68063420da0c46f95716ac2d9c
Martyna Gil <martynaa.gil@gmail.com>
2021-02-23 00:53:13 +0100

    implemented product type

--- src/main/java/com/github/martynagil/drugstoremanagement/controller/ProductTypeController.java
+++ src/main/java/com/github/martynagil/drugstoremanagement/controller/ProductTypeController.java
@@ -1,7 +1,24 @@
 package com.github.martynagil.drugstoremanagement.controller;
 
+import com.github.martynagil.drugstoremanagement.dto.ProductTypeDto;
+import com.github.martynagil.drugstoremanagement.service.ProductTypeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("product-types")
 public class ProductTypeController {
+
+    ProductTypeService productTypeService;
+
+    public ProductTypeController(ProductTypeService productTypeService) {
+        this.productTypeService = productTypeService;
+    }
+
+    @PostMapping
+    public void addProductType(@RequestBody ProductTypeDto productTypeDto) {
+        productTypeService.addProductType(productTypeDto);
+    }
 }
--- src/main/java/com/github/martynagil/drugstoremanagement/service/ProductTypeService.java
+++ src/main/java/com/github/martynagil/drugstoremanagement/service/ProductTypeService.java
@@ -1,7 +1,35 @@
 package com.github.martynagil.drugstoremanagement.service;
 
+import com.github.martynagil.drugstoremanagement.dto.ProductTypeDto;
+import com.github.martynagil.drugstoremanagement.exceptions.ProductTypeAlreadyExistsException;
+import com.github.martynagil.drugstoremanagement.model.ProductType;
+import com.github.martynagil.drugstoremanagement.repositories.ProductTypeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductTypeService {
+
+    ProductTypeRepository productTypeRepository;
+
+    public ProductTypeService(ProductTypeRepository productTypeRepository) {
+        this.productTypeRepository = productTypeRepository;
+    }
+
+
+    public void addProductType(ProductTypeDto productTypeDto) {
+        if (productTypeExists(productTypeDto)) {
+            throw new ProductTypeAlreadyExistsException();
+        }
+
+        ProductType productType = createProductTypeFromDto(productTypeDto);
+        productTypeRepository.save(productType);
+    }
+
+    private ProductType createProductTypeFromDto(ProductTypeDto productTypeDto) {
+        return new ProductType(productTypeDto.getName());
+    }
+
+    private boolean productTypeExists(ProductTypeDto productTypeDto) {
+        return productTypeRepository.existsByName(productTypeDto.getName());
+    }
 }
