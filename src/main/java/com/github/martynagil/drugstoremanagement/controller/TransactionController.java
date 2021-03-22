82e3b34b9ad6c76c8250b3395e4c627afae89d1b
Martyna Gil <martynaa.gil@gmail.com>
2021-03-22 15:33:02 +0100

    implemented transaction controller, dto and service

--- src/main/java/com/github/martynagil/drugstoremanagement/controller/TransactionController.java
+++ src/main/java/com/github/martynagil/drugstoremanagement/controller/TransactionController.java
@@ -1,7 +1,24 @@
 package com.github.martynagil.drugstoremanagement.controller;
 
+import com.github.martynagil.drugstoremanagement.dto.TransactionDto;
+import com.github.martynagil.drugstoremanagement.service.TransactionService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("transactions")
 public class TransactionController {
+
+    private TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public void addTransaction(@RequestBody TransactionDto transactionDto) {
+        transactionService.addTransaction(transactionDto);
+    }
 }
--- src/main/java/com/github/martynagil/drugstoremanagement/service/TransactionService.java
+++ src/main/java/com/github/martynagil/drugstoremanagement/service/TransactionService.java
@@ -1,7 +1,34 @@
 package com.github.martynagil.drugstoremanagement.service;
 
+import com.github.martynagil.drugstoremanagement.dto.TransactionDto;
+import com.github.martynagil.drugstoremanagement.model.Transaction;
+import com.github.martynagil.drugstoremanagement.repositories.ShopRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 public class TransactionService {
+
+    private ShopRepository shopRepository;
+
+    public TransactionService(ShopRepository shopRepository) {
+        this.shopRepository = shopRepository;
+    }
+
+    @Transactional
+    public void addTransaction(TransactionDto transactionDto) {
+        Transaction transaction = createTransactionFromDto(transactionDto);
+    }
+
+    private Transaction createTransactionFromDto(TransactionDto transactionDto) {
+        return new Transaction(
+                transactionDto.getSubmissionTime(),
+                shopRepository.findById(transactionDto.getShopId())
+                .orElseThrow(EntityNotFoundException::new),
+                transactionDto.getTransactionEntries()
+        );
+    }
+
 }
